f852c63f33529270bd64f34a41ab1bb3e15fa235
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2022-10-09 14:36:33 -0700

    Update Climbing Stairs.py

--- blind-75/dp/Climbing Stairs.py
+++ blind-75/dp/Climbing Stairs.py
@@ -13,7 +13,7 @@ class Solution:
         if self.step_count.get(n):
             return  self.step_count[n]
         
-         self.step_count[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        self.step_count[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
    
     
         return  self.step_count[n]
