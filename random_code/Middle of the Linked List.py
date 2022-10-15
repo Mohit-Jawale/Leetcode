c30d7f3699a82ed1a4274e37f60188ca1ac45180
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2022-10-14 19:06:27 -0700

    Update Middle of the Linked List.py

--- random_code/Middle of the Linked List.py
+++ random_code/Middle of the Linked List.py
@@ -1,3 +1,23 @@
+
+This is one is better solution 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        slow_ptr = head
+        fast_ptr = head
+        
+        while fast_ptr!=None and fast_ptr.next!=None:
+            fast_ptr = fast_ptr.next.next
+            slow_ptr = slow_ptr.next    
+        return slow_ptr
+
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
