999dd7b0e32e104bbc59880e2c946ec7b48e06eb
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2022-12-11 18:04:59 -0800

    Update two_sum_naive.py

--- Amazon/two_sum_naive.py
+++ Amazon/two_sum_naive.py
@@ -18,3 +18,20 @@ class Solution(object):
                     ans.append(count_2-1)
                     
         return set(ans)
+
+ ####### In the Revision of concepts I found out that the above solution is complicated so wrote new navie solution.
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+
+            ans = []
+            for i in range(len(nums)):
+                for j in range(i+1,len(nums)):
+                    if nums[i]+nums[j]==target:
+                        ans.append(i)
+                        ans.append(j)
+                        break
+
+            return ans
+    
