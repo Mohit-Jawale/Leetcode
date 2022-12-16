cbdcf0494a24fff758a0b8b38e84bf7f74bd1ee2
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2022-12-16 12:41:53 -0800

    Update Sort Colors.py

--- top_150_questions/sorting/Sort Colors.py
+++ top_150_questions/sorting/Sort Colors.py
@@ -2,7 +2,10 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        
         """
+        ###########This is O(2N) solution
+        
         red_count = 0
         white_count = 0
         blue_count = 0
@@ -25,6 +28,22 @@ class Solution:
                 nums[i]=2
                 blue_count-=1
 
-        return(nums)                
+        return(nums)
+    ####################This is more efficent solution with O(N) complexity
+        p0 = curr = 0
+        p2 = len(nums)-1
+
+        while curr<=p2:
+            if nums[curr]==0:
+                nums[curr], nums[p0] = nums[p0],nums[curr]
+                p0+=1
+                curr+=1
+            elif nums[curr] ==2:
+                nums[curr], nums[p2] = nums[p2],nums[curr]
+                p2-=1
+            else:
+                curr+=1
+
+        return nums       
 
 
